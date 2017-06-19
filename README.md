bd8f59e60fa7c46c7a92f1bf42f21f105739f320
Dmitry Kochetov <aziris_nuna@list.ru>
2017-06-19 20:40:05 +0300

    fix

--- README.md
+++ README.md
@@ -11,34 +11,11 @@ CREATE TABLE `users` (
      `password` varchar(128) NOT NULL DEFAULT '' COMMENT 'User’s password (hashed).',
      `email` varchar(254) DEFAULT '' COMMENT 'User’s e-mail address.',
      `name` varchar(60) NOT NULL DEFAULT '' COMMENT 'user name.',
+     `surname` varchar(60) NOT NULL DEFAULT '' COMMENT 'user name.',
        PRIMARY KEY (`uid`),
-       UNIQUE KEY `login` (`login`)
+       UNIQUE KEY `login` (`login`),
+       UNIQUE KEY `email` (`email`)
      ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Stores user data.';
-
-CREATE TABLE `interest` (
-
-     `id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT 'Primary Key: Unique interests  ID.',
-     `text` varchar(60) NOT NULL DEFAULT '' COMMENT 'user name.',
-       PRIMARY KEY (`id`)
-     ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Stores interests data.';
-
-
-CREATE TABLE `interests` (
-
-     `interest_id` int(10) unsigned NOT NULL  COMMENT 'foreign Key: Unique interests  ID.',
-     `uid` int(10) unsigned NOT NULL  COMMENT 'foreign Key: Unique user ID.',
-       FOREIGN KEY (uid) REFERENCES users(uid)  ON DELETE CASCADE,
-       FOREIGN KEY (interest_id) REFERENCES interest(id)  ON DELETE CASCADE
-     ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Stores interests data.';
-
-CREATE TABLE `friends` (
-
-     `uid` int(10) unsigned NOT NULL  COMMENT 'foreign Key: Unique user ID.',
-     `friend_id` int(10) unsigned NOT NULL  COMMENT 'foreign Key: Unique interests  ID.',
-       FOREIGN KEY (uid) REFERENCES users(uid)  ON DELETE CASCADE,
-       FOREIGN KEY (friend_id) REFERENCES users(uid)  ON DELETE CASCADE
-     ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Stores interests data.';
-
 
 
 GET /profile/<user_id>
@@ -50,34 +27,7 @@ GET /profile/<user_id>
 {
 
         "email": "3@mail.ru",   
-        "friends": [  
-                {    
-                        "login": "dima",       
-                        "uid": 2      
-                },     
-                {    
-                        "login": "masha",       
-                        "uid": 8      
-                }    
-                ],   
-        "interests": [  
-                {    
-                        "id": 2,       
-                        "text": "Baby"      
-                },     
-                {    
-                        "id": 5,       
-                        "text": "Computer"      
-                },     
-                {    
-                        "id": 3,       
-                        "text": "Sport"      
-                },     
-                {    
-                        "id": 4,      
-                        "text": "Science"      
-                }
-                ],   
+        "surname": "", 
         "login": "vika",  
         "name": "\u0412\u0438\u043a\u0430",   
         "password": "eccbc87e4b5ce2fe28308fd9f2a7baf3",   
@@ -93,25 +43,31 @@ GET /profile/<user_id>
         'error': 'Not found'        
 }
 
+Создание профиля
 POST /auth/registration
 
 Param:
 
 {
 
-	"login":"ilya",        
-	"password":"a87ff679a2f3e71d9181a67b7542122c",        
-	"name":"Илья",        
-	"email":"3@mail.ru",        
+	"login":"ilya",
+	"password":"a87ff679a2f3e71d9181a67b7542122c",
+	"name":"Илья",
+	"surname": "Кочетов"
+	"email":"3@mail.ru",
 }
 
 
 Ответ:
 
 HTTP/1.0 201 CREATED
+
 Content-Type: application/json
+
 Content-Length: 16
+
 Server: Werkzeug/0.12.2 Python/2.7.12
+
 Date: Sun, 18 Jun 2017 09:45:52 GMT
 
 {
@@ -127,4 +83,46 @@ Date: Sun, 18 Jun 2017 09:45:52 GMT
         'error': 'Not found'
 }
 
+
+Изменение профиля
+
+POST /profile
+
+Param:
+
+{
+	
+	"profile_id": 9,
+	"login":"ilya",
+	"password":"a87ff679a2f3e71d9181a67b7542122c",
+	"name":"Илья",
+	"surname": "Кочетов"
+	"email":"3@mail.ru",
+}
+
+
+Ответ:
+
+HTTP/1.0 200 OK
+
+Content-Type: application/json
+
+Content-Length: 16
+
+Server: Werkzeug/0.12.2 Python/2.7.12
+
+Date: Sun, 18 Jun 2017 09:45:52 GMT
+
+{
+
+        "uid": 31  
+}
+
+
+Ошибка:
+
+{
+        
+        'error': 'Not found'
+}
 
--- app1.py
+++ app1.py
@@ -3,11 +3,14 @@ from flask import Flask, jsonify, abort
 import MySQLdb
 import string
 
+from flask import make_response
+from flask import request
+
 app = Flask(__name__)
 
-@app.route('/todo/api/v1.0/tasks', methods=['GET'])
-def get_users():
-    return jsonify({'tasks': tasks})
+# @app.route('/todo/api/v1.0/tasks', methods=['GET'])
+# def get_users():
+#     return jsonify({'tasks': tasks})
 
 @app.route('/profile/<int:user_id>', methods=['GET'])
 def get_task(user_id):
@@ -17,41 +20,129 @@ def get_task(user_id):
     sql = """SELECT * FROM users WHERE uid='%(user_id)s'"""%{'user_id':user_id}
     cursor.execute(sql)
     data = cursor.fetchall()
-    sql = """SELECT DISTINCT  ins.uid,ins.interest_id,inter.text FROM interests ins JOIN interest inter ON inter.id=ins.interest_id WHERE uid='%(user_id)s'"""%{'user_id':user_id}
-    cursor.execute(sql)
-    data_inter = cursor.fetchall()
-    sql = """SELECT DISTINCT  friends.friend_id,users.login FROM users JOIN friends  ON users.uid=friends.friend_id WHERE friends.uid='%(user_id)s' """%{'user_id':user_id}
-    cursor.execute(sql)
-    data_friend = cursor.fetchall()
-    sql = """SELECT DISTINCT  friends.uid,users.login FROM users JOIN friends  ON users.uid=friends.uid WHERE friends.friend_id='%(user_id)s'"""%{'user_id':user_id}
-    cursor.execute(sql)
-    data_friend1 = cursor.fetchall()
     user = dict()
-
     for rec in data:
-        uid, login, password,email, name = rec
+        uid, login, password,email, name, surname = rec
         user['uid'] = uid
         user['login'] = login
         user['password']=password
         user['email']=email
         user['name']=unicode(name)
-        interests = []
-        for inter in data_inter:
-            interests.append({'id':inter[1],'text':inter[2]})
-        user['interests'] = interests
-        friends = []
-        for friend in data_friend1+data_friend:
-            friends.append({'uid':friend[0],'login':friend[1]})
-        user['friends'] = friends
-        #print unicode(name)
-
+        user['surname'] = unicode(surname)
     db.close()
 
     if len(user) == 0:
         abort(404)
     return jsonify(user)
 
-from flask import make_response
+
+@app.route('/auth/registration', methods=['POST'])
+def create_profile():
+    if not request.json:
+        abort(400)
+    if 'login'  in request.json and type(request.json['login']) != unicode:
+        abort(400)
+    if 'password'  in request.json and type(request.json['password']) is not unicode:
+        abort(400)
+    if 'name'  in request.json and type(request.json['name']) is not unicode:
+        abort(400)
+    if 'surname'  in request.json and type(request.json['surname']) is not unicode:
+        abort(400)
+    if 'email'  in request.json and type(request.json['email']) is not unicode:
+        abort(400)
+
+    if 'login'  in request.json:
+        login = request.json.get('login')
+    else:
+        abort(400)
+    if 'password'  in request.json:
+        password = request.json.get('password')
+    else:
+        abort(400)
+    if 'name'  in request.json:
+        name = request.json.get('name')
+    else:
+        name = ''
+    if 'surname'  in request.json:
+        surname = request.json.get('surname')
+    else:
+        surname = ''
+    if 'email'  in request.json:
+        email = request.json.get('email')
+    else:
+        abort(400)
+
+    db = MySQLdb.connect(host="localhost", user="root", passwd="root", db="users", charset='utf8')
+    cursor = db.cursor()
+
+    sql = """INSERT INTO users (login,password,name,surname,email) VALUE ('%(login)s','%(password)s','%(name)s','%(surname)s','%(email)s')""" % \
+          {'login': login,'password': password,'name': name,'surname':surname,'email': email}
+    cursor.execute(sql)
+    uid = cursor.lastrowid
+    db.commit()
+    db.close()
+
+    return jsonify({'uid': uid}), 201
+
+
+@app.route('/profile', methods=['POST'])
+def edit_profile():
+    if not request.json:
+        abort(400)
+    if 'login'  in request.json and type(request.json['login']) != unicode:
+        abort(400)
+    if 'password'  in request.json and type(request.json['password']) is not unicode:
+        abort(400)
+    if 'name'  in request.json and type(request.json['name']) is not unicode:
+        abort(400)
+    if 'surname'  in request.json and type(request.json['surname']) is not unicode:
+        abort(400)
+    if 'email'  in request.json and type(request.json['email']) is not unicode:
+        abort(400)
+    if 'profile_id' in request.json and type(request.json['profile_id']) is not int:
+        abort(400)
+
+    if 'profile_id'  in request.json:
+        profile_id = request.json.get('profile_id')
+    else:
+        abort(400)
+    if 'login'  in request.json:
+        login = request.json.get('login')
+    else:
+        abort(400)
+    if 'password'  in request.json:
+        password = request.json.get('password')
+    else:
+        abort(400)
+    if 'name'  in request.json:
+        name = request.json.get('name')
+    else:
+        name = ''
+    if 'surname'  in request.json:
+        surname = request.json.get('surname')
+    else:
+        surname = ''
+    if 'email'  in request.json:
+        email = request.json.get('email')
+    else:
+        abort(400)
+
+    db = MySQLdb.connect(host="localhost", user="root", passwd="root", db="users", charset='utf8')
+    cursor = db.cursor()
+    sql = """SELECT * FROM users WHERE uid='%(user_id)s'"""%{'user_id':profile_id}
+    cursor.execute(sql)
+    data = cursor.fetchall()
+    if len(data)==0:
+        abort(404)
+    sql = """ UPDATE users SET  login='%(login)s',password='%(password)s',name='%(name)s',surname='%(surname)s',email='%(email)s' WHERE uid='%(uid)s'""" % \
+          {'login': login,'password': password,'name': name,'surname':surname,'email': email,'uid':profile_id}
+    cursor.execute(sql)
+    uid = cursor.lastrowid
+    db.commit()
+    db.close()
+
+    return jsonify({'uid': profile_id}), 200
+
 
 @app.errorhandler(404)
 def not_found(error):
@@ -61,43 +152,6 @@ def not_found(error):
 def not_found(error):
     return make_response(jsonify({'error': 'Bad request'}), 400)
 
-from flask import request
-
-@app.route('/auth/registration', methods=['POST'])
-def create_task():
-    if not request.json:
-        abort(400)
-    if 'login'  in request.json and type(request.json['login']) != unicode:
-        abort(400)
-    if 'password'  in request.json and type(request.json['password']) is not unicode:
-        abort(400)
-    if 'name'  in request.json and type(request.json['name']) is not unicode:
-        abort(400)
-    if 'email'  in request.json and type(request.json['email']) is not unicode:
-        abort(400)
-    # if 'interests' in request.json and type(request.json['interests']) is not list:
-    #     abort(400)
-    db = MySQLdb.connect(host="localhost", user="root", passwd="root", db="users", charset='utf8')
-    cursor = db.cursor()
-    sql = """INSERT INTO users (login,password,name,email) VALUE ('%(login)s','%(password)s','%(name)s','%(email)s')""" % \
-          {'login': request.json.get('login'),'password': request.json.get('password'),'name': request.json.get('name'),'email': request.json.get('email')}
-    cursor.execute(sql)
-    uid = cursor.lastrowid
-    # interests = request.json.get('interests')
-    # print interests
-    #
-    # for interest in interests:
-    #     print interest['text']
-    #     sql = """SELECT * FROM interest WHERE text like '%(text)s' """%{'text':interest['text']}
-    #     cursor.execute(sql)
-    #     interest_id=cursor.fetchall()
-    #     for id in interest_id:
-    #         print id
-    #         cursor.execute("""INSERT INTO interests (interest_id,uid) VALUE ('%(interest_id)s','%(uid)s')"""%{'interest_id':id[0],'uid':uid})
-    db.commit()
-    db.close()
-
-    return jsonify({'uid': uid}), 201
 
 if __name__ == '__main__':
     app.run(debug=True)
